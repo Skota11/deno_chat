1333b07acbcf9181004ef218db44a6c1aff4ce9e
S Kota <91359399+Skota11@users.noreply.github.com>
2023-02-21 20:47:03 +0900

    bcrypt_test

--- util/bcrypt.ts
+++ util/bcrypt.ts
@@ -7,11 +7,11 @@ import {
 
 export const isRunningInDenoDeploy = Deno.permissions?.query === undefined; // This is crude check for if the code in running in Deno Deploy. It works for now but may not work in the future.
 
-export const hash: typeof hashPromise = isRunningInDenoDeploy
+export const hash_ut: typeof hashPromise = isRunningInDenoDeploy
   ? (plaintext: string, salt: string | undefined = undefined) =>
     new Promise((res) => res(hashSync(plaintext, salt)))
   : hashPromise;
-export const compare: typeof comparePromise = isRunningInDenoDeploy
+export const compare_ut: typeof comparePromise = isRunningInDenoDeploy
   ? (plaintext: string, hash: string) =>
     new Promise((res) => res(compareSync(plaintext, hash)))
   : comparePromise;
