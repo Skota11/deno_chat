48414ef9dcddd98618f063433b59f1bc4a2e54cb
S Kota <91359399+Skota11@users.noreply.github.com>
2023-02-25 17:00:30 +0900

    socket.test

--- islands/chat.tsx
+++ islands/chat.tsx
@@ -2,44 +2,42 @@ import { io } from "https://cdn.socket.io/4.4.1/socket.io.esm.min.js";
 import { useState } from "preact/hooks";
 
 export default function MyComponent(props: { data: [] }) {
-  const [messages, setMsgObject] = useState([]);
+  const [messages, setMsgObject] = useState(0);
 
-  const socket = io("wss://socket.skota11.com");
+  const socket = io("wss://socket-mongrop.onrender.com");
 
   socket.on("connect", () => {
-    console.log(socket.connected); // true
-
     socket.emit("login", props.data);
 
     socket.on("log", (msg) => {
-      const Obj = messages;
-      Obj.push(msg);
+      const Obj = messages + 1;
       setMsgObject(Obj);
     });
   });
 
   const pressEnter = (e: any) => {
-    // ここでエンターキーを指定する
     if (e.key == "Enter") {
       document.getElementById("content").innerHTML = "";
       return e.preventDefault();
     }
   };
-  console.log(messages);
   return (
     <>
       <div>
         <div class="w-48 rounded-md shadow-2xl h-screen bg-cloudy w-full float-right">
-          <p>TEST</p>
         </div>
         <div class="">
           <div id="msglist">
-            {messages.map((message) => {
+            {
+              /* {messages.map((message) => {
+              console.log(message);
               if (message.type == 0) {
                 return <p>{message.content}</p>;
               } else if (message.type == 1) {
               }
-            })}
+            })} */
+            }
+            {messages}
           </div>
           <div class="fixed w-10/12 bottom-0 mb-4 rounded-md w-full h-auto bg-cloudy">
             <div class="inline-block m-auto w-full h-auto">
--- socket.io/index.js
+++ socket.io/index.js
@@ -23,6 +23,8 @@ io.on("connection", (socket)=>{
     socket.display_name = msg.display_name;
     socket.img = msg.img_url;
 
+    console.log(socket)
+
     socket.emit("log",{type:0,content:`@${socket.name}が入室しました。`})
   });
 });
